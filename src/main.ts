a8d3df27c7bf179fc2e73e96b7a29b95050e0c19
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2025-09-11 17:28:46 +0300

    chore: Init with pnpm
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+// Copyright 2021-2025 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+import * as core from '@actions/core'
+
+async function run(): Promise<void> {
+  core.info(`Cerbos Action`)
+}
+
+run()
